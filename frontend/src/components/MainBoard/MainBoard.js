c2c00d5add21f641d77480b7d49b932093ecfdc7
Darmun <darmun@protonmail.com>
2019-02-13 21:01:25 +0100

    structuring basic components

---
+++ frontend/src/components/MainBoard/MainBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainBoard.css';
+
+export default class MainBoard extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(<div className="main-board">potnę się żyletkami</div>)
+    }
+}
