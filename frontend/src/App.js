54eeae50f697fdfd5d39e04ce39b8c86ff0788dc
Darmun <darmun@protonmail.com>
2019-02-14 14:07:58 +0100

    installed material ui

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{height:'100%'}}>
+    <div>instrument</div>
+    <div style={{backgroundColor:'#2f4870'}}>menu</div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MainBoard from './components/MainBoard/MainBoard';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<MainBoard/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
