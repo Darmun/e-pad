860b60d020c3def27edbe0094efcedb3f622e9ef
Darmun <darmun@protonmail.com>
2019-02-14 15:28:13 +0100

    static sizing of Pad

--- frontend/src/components/Instrument/Instrument.js
+++ frontend/src/components/Instrument/Instrument.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Instrument.css';
-export  class Instrument extends React.Component {
-    
+export class Instrument extends React.Component {
+
     constructor(props) {
         super(props);
     }
 
     render() {
-        return(<div className='instrument-container'>instrument</div>)
+        const boardSize = {
+            gridTemplateColumns: `repeat(${this.props.size.columns}, 1fr)`,
+            gridTemplateRows:`repeat(${this.props.size.rows}, 1fr)`
+        }
+        return (<div className='instrument-container'>
+            <div className='instrument' style={boardSize}></div>
+        </div>)
     }
 }
