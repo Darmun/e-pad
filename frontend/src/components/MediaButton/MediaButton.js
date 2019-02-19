6ad2d650fcb7cfd1ae9030bedf7b0467ddf2a6d7
Darmun <darmun@protonmail.com>
2019-02-19 14:49:23 +0100

    added light up when sample is played

--- frontend/src/components/MediaButton/MediaButton.js
+++ frontend/src/components/MediaButton/MediaButton.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import './MediaButton.css';
 // volume control
+
+// onended
+//onplay
+
 class MediaButton extends React.Component {
     constructor(props) {
         super(props);
         this.audio = new Audio();
         this.playSample = this.playSample.bind(this);
+        this.state = { backgroundColor: '' };
+        this.audio.onended = () => {
+            this.setState({ backgroundColor: '' });
+        }
+
     }
 
     playSample() {
 
         this.audio.play();
+        this.audio.onplay = () => {
+            this.setState({ backgroundColor: 'rgba(0, 220, 0,0.6)' });
+        }
+    }
+    componentDidMount() {
+        this.audio.src = this.props.audioSample;
     }
 
     render() {
         let volumeValue = this.props.volume / 100;
-        let track = this.props.audioSample;
         this.audio.volume = volumeValue;
-        this.audio.src = track;
-        const active = {
-            cursor: 'pointer',
-        }
-        const inactive = {
-            backgroundColor: 'grey',
-        }
+
         return (
-            <button className="pad-button" style={this.props.isActive ? active : inactive} onClick={this.playSample}></button>
+            <button className={`pad-button ${this.props.isActive ? 'active' : 'inactive'}`} style={this.state} onClick={this.playSample}></button>
         )
     }
 }
