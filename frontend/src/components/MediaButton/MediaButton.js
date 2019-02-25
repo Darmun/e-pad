f8efb316223654b9af54727924c898a906e17e30
Darmun <darmun@protonmail.com>
2019-02-25 11:00:20 +0100

    fixed "ghost" samples

--- frontend/src/components/MediaButton/MediaButton.js
+++ frontend/src/components/MediaButton/MediaButton.js
@@ -5,11 +5,15 @@ class MediaButton extends React.Component {
     constructor(props) {
         super(props);
         this.audio = new Audio();
-        this.state = { playing : false };
+        this.state = {
+            playing: false,
+            bindedBtn: ''
+        };
         this.playSample = this.playSample.bind(this);
         this.handleClick = this.handleClick.bind(this);
         this.bindButton = this.bindButton.bind(this);
         this.chooseButton = this.chooseButton.bind(this);
+        this.triggerBtn = this.triggerBtn.bind(this);
         this.audio.onended = () => {
             this.setState({ playing: false });
         }
@@ -24,16 +28,12 @@ class MediaButton extends React.Component {
             this.playSample();
         }
     }
-
     bindButton(e) {
         console.log('Binding button...')
         const key = e.keyCode;
-        document.addEventListener('keydown', e => {
-            if (e.keyCode === key) {
-                this.playSample();
-            }
-            document.removeEventListener('keydown', this.bindButton, false);
-        }, false)
+        this.setState({ bindedBtn: key });
+        document.addEventListener('keydown',this.triggerBtn);
+        document.removeEventListener('keydown', this.bindButton, false);
     }
 
     chooseButton() {
@@ -44,24 +44,33 @@ class MediaButton extends React.Component {
         if (this.props.isActive) {
             this.audio.play();
             this.audio.onplay = () => {
-                this.setState({playing: true });
+                this.setState({ playing: true });
             }
         }
         else {
             return false;
         }
     }
+
+     triggerBtn(e) {
+        if (e.keyCode === this.state.bindedBtn) {
+            this.playSample();
+        }
+
+    }
+
     componentDidMount() {
         this.audio.src = this.props.audioSample;
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         document.removeEventListener('keydown', this.bindButton, false);
+        document.removeEventListener('keydown', this.triggerBtn);
     }
 
     render() {
         this.audio.volume = this.props.volume / 100;
-const OnOffStyle = this.state.playing ? { backgroundColor: 'rgba(0, 220, 0,0.6)' } : { backgroundColor: '' };
+        const OnOffStyle = this.state.playing ? { backgroundColor: 'rgba(0, 220, 0,0.6)' } : { backgroundColor: '' };
         return (
             <button className="pad-button" disabled ={!this.props.isActive} style={OnOffStyle} onClick={this.handleClick}></button>
         )
